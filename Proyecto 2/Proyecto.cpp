f330ff79652a360089eaff4487a76cae37d3eb85
DiegoYaxcal <137358185+DiegoYaxcal@users.noreply.github.com>
2023-06-21 18:04:35 -0600

    Add files via upload

---
+++ Proyecto 2/Proyecto.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int fila=4;
+	int columna=4;
+	
+	int matriz[fila][columna];
+	
+	for(int i=0;i<fila;i++){
+		for(int x=0;x<columna;x++){
+			cout<<"ingrese el dato en "<<i<<","<<x<<endl;
+			cin>>matriz[i][x];
+		}
+	}
+	
+	cout<<"resultados: ";
+	
+	for(int i=0;i<fila;i++){
+		int suma=0;
+		for(int a=0;a<columna;a++){
+		suma +=matriz[i][a];
+    	}
+    	cout<<"la suma de las filas es: "<<i+1<<suma<<endl;
+	}
+	
+	for(int i=0;i<fila;i++){
+		int suma2=0;
+		for(int a=0;a<columna;a++){
+		suma2 +=matriz[i][a];
+    	}
+    	cout<<"la suma de las columnas es: "<<i+1<<suma2<<endl;
+	}
+
+	return 0;
